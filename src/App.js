b9fcde35774073faad32ff730b62d6f16c88dd91
Eng-Moataz-M-Khaled <moatazmohamed1310@gmail.com>
2026-03-24 03:38:28 +0200

    exposed and fixing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hopefully it works this time
+          one more time
         </p>
         <a
           className="App-link"
